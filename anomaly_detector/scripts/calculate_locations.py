4624afb610aa3dbfe22ca3a2705fbbad3b9d7d0c
LdwgWffnschmdt <LudwigWW@live.com>
2020-03-18 12:28:52 +0100

    Improvements to script

--- anomaly_detector/scripts/calculate_locations.py
+++ anomaly_detector/scripts/calculate_locations.py
@@ -1,21 +1,26 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import consts
 import argparse
 
-parser = argparse.ArgumentParser(description="A small script to add patch locations to feature files.",
+parser = argparse.ArgumentParser(description="Add patch locations to feature files.",
                                  formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("files", metavar="F", type=str, nargs='+',
+parser.add_argument("--files", metavar="F", dest="files", type=str, nargs='*', default=consts.FEATURES_FILES,
                     help="The feature file(s). Supports \"path/to/*.h5\"")
 
 args = parser.parse_args()
 
 import os
+import sys
 import time
-import logging
+import traceback
 from glob import glob
 
+from tqdm import tqdm
+
+import common.logger as logger
 import common.utils as utils
 from common import FeatureArray
 
@@ -29,25 +34,34 @@ def calculate_locations():
     if not files or len(files) < 1 or files[0] == "":
         raise ValueError("Please specify at least one filename (%s)" % files)
     
+    if isinstance(files, basestring):
+        files = [files]
+        
     # Expand wildcards
     files_expanded = []
     for s in files:
         files_expanded += glob(s)
     files = list(set(files_expanded)) # Remove duplicates
 
-    for features_file in files:
+    for features_file in tqdm(files, file=sys.stderr):
         # Check parameters
         if features_file == "" or not os.path.exists(features_file) or not os.path.isfile(features_file):
-            logging.error("Specified feature file does not exist (%s)" % features_file)
+            logger.error("Specified feature file does not exist (%s)" % features_file)
             return
 
-        logging.info("Calculating locations for %s" % features_file)
+        logger.info("Calculating locations for %s" % features_file)
         
-        # Load the file
-        features = FeatureArray(features_file)
+        try:
+            # Load the file
+            features = FeatureArray(features_file)
 
-        # Calculate and save the locations
-        features.save_locations_to_file()
+            # Calculate and save the locations
+            features.save_locations_to_file()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            logger.error("%s: %s" % (features_file, traceback.format_exc()))
 
 if __name__ == "__main__":
     calculate_locations()
+    pass
