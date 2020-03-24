5a6aa1e95c7bd9d59889fd49bbfe8386d182d4c8
LdwgWffnschmdt <LudwigWW@live.com>
2020-03-24 13:56:53 +0100

    Improved logger

--- anomaly_detector/common/logger.py
+++ anomaly_detector/common/logger.py
@@ -1,105 +1,121 @@
-if "LOGGER_LOADED" not in globals():
-    LOGGER_LOADED = True
+#   Level | Level for Humans | Level Description                  
+#  -------|------------------|------------------------------------ 
+#   0     | DEBUG            | [Default] Print all messages       
+#   1     | INFO             | Filter out INFO messages           
+#   2     | WARNING          | Filter out INFO & WARNING messages 
+#   3     | ERROR            | Filter out all messages  
+import os
+import logging
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
-    import logging
-    import sys
+import tensorflow as tf
+tf_logger = tf.get_logger()
+tf_logger.setLevel(logging.ERROR)
+for i in range(len(tf_logger.handlers)):
+    tf_logger.removeHandler(tf_logger.handlers[i])
 
-    import tqdm
-    import colorlog
+import sys
 
-    #   Level | Level for Humans | Level Description                  
-    #  -------|------------------|------------------------------------ 
-    #   0     | DEBUG            | [Default] Print all messages       
-    #   1     | INFO             | Filter out INFO messages           
-    #   2     | WARNING          | Filter out INFO & WARNING messages 
-    #   3     | ERROR            | Filter out all messages  
-    import os
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import tqdm
+import colorlog
 
-    import tensorflow as tf
+class TqdmLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        logging.Handler.__init__(self, level)
 
-    # Configure logging
-    class TqdmLoggingHandler(logging.Handler):
-        def __init__(self, level=logging.NOTSET):
-            logging.Handler.__init__(self, level)
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.tqdm.write(msg, file=sys.stdout)
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
 
-        def emit(self, record):
-            try:
-                msg = self.format(record)
-                tqdm.tqdm.write(msg, file=sys.stdout)
-                self.flush()
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except:
-                self.handleError(record)
+class TqdmFile(object):
+    file = None
+    def __init__(self, file):
+        self.file = file
 
-    logger = tf.get_logger()
-    logger.removeHandler(logger.handlers[0])
-    logger.setLevel(logging.INFO)
+    def write(self, x):
+        # Avoid print() second call (useless \n)
+        if len(x.rstrip()) > 0:
+            tqdm.tqdm.write(x, file=self.file)
 
-    handler = TqdmLoggingHandler()
+    def __eq__(self, other):
+        return other is self.file
 
-    formatter = colorlog.ColoredFormatter(
-            '%(log_color)s%(asctime)s %(levelname).1s: %(message)s',
-            datefmt='%Y-%d-%d %H:%M:%S',
-            log_colors={
-                'DEBUG': 'thin_cyan',
-                'INFO': 'thin_white',
-                'SUCCESS:': 'green',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'red,bg_white'},)
+class Logger(object):
+    def __init__(self):
+        self.logger = logging.getLogger("root")
+        self.logger.propagate = False
+        
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.INFO)
+            handler = TqdmLoggingHandler()
+            
+            formatter = colorlog.ColoredFormatter(
+                '%(log_color)s%(asctime)s %(levelname).1s: %(message)s',
+                datefmt='%Y-%d-%d %H:%M:%S',
+                log_colors={
+                    'DEBUG': 'thin_cyan',
+                    'INFO': 'thin_white',
+                    'SUCCESS:': 'green',
+                    'WARNING': 'yellow',
+                    'ERROR': 'red',
+                    'CRITICAL': 'red,bg_white'},)
 
-    handler.setFormatter(formatter)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
 
-    logger.addHandler(handler)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    # Redirect
+    def info(self, msg):
+        self.logger.info(msg)
 
-    class DummyFile(object):
-        file = None
-        def __init__(self, file):
-            self.file = file
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-        def write(self, x):
-            # Avoid print() second call (useless \n)
-            if len(x.rstrip()) > 0:
-                tqdm.tqdm.write(x, file=self.file)
+    def error(self, msg):
+        self.logger.error(msg)
 
-        def __eq__(self, other):
-            return other is self.file
+# Redirect
+sys.stdout = TqdmFile(sys.stdout)
 
-    sys.stdout = DummyFile(sys.stdout)
+logger = Logger()
 
-    debug = logger.debug
-    info = logger.info
-    warning = logger.warning
-    error = logger.error
-    exception = logger.exception
-    critical = logger.critical
-    fatal = critical
-    log = logger.log
+debug = logger.debug
+info = logger.info
+warning = logger.warning
+error = logger.error
 
-    if __name__ == "__main__":
-        import time
+if __name__ == "__main__":
+    import time
+    import consts
 
-        for i in tqdm.tqdm(range(100), desc="Progress", file=sys.stderr):
-            if i == 5:
-                logger.info("HALLO")
-            if i == 10:
-                logger.warning("HALLO")
-            if i == 15:
-                logger.error("HALLO")
-            if i == 20:
-                logger.debug("HALLO")
-            if i == 25:
-                print("PUP")
-            if i == 30:
-                tqdm.tqdm.write("HALLALA")
-            if i == 45:
-                from feature_extractor import FeatureExtractorC3D
-                FeatureExtractorC3D()
-            if i == 40:
-                for x in tqdm.tqdm(range(30), desc="Subprogress", file=sys.stderr):
-                    time.sleep(0.05)
-            time.sleep(0.1)
+    for i in tqdm.tqdm(range(100), desc="Progress", file=sys.stderr):
+        if i == 5:
+            logger.info("HALLO")
+        if i == 10:
+            logger.warning("HALLO")
+        if i == 15:
+            __import__("tensorflow")
+            logger.error("HALLO")
+        if i == 20:
+            __import__("tensorflow_hub")
+            logger.debug("HALLO")
+        if i == 25:
+            print("PUP")
+        if i == 30:
+            tqdm.tqdm.write("HALLALA")
+        if i == 40:
+            for x in tqdm.tqdm(range(30), desc="Subprogress", file=sys.stderr):
+                time.sleep(0.02)
+        if i == 45:
+            from feature_extractor import FeatureExtractorEfficientNetB0
+            extractor = FeatureExtractorEfficientNetB0()
+        if i == 50:
+            extractor.extract_files(consts.EXTRACT_FILES_TEST)
+        time.sleep(0.1)
