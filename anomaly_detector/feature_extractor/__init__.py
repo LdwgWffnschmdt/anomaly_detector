c7da8d07bfccb0ed51cfad5c0edd4ce1c9d5f9d2
LdwgWffnschmdt <LudwigWW@live.com>
2020-03-31 09:00:59 +0200

    Exclude storage heavy extractors

--- anomaly_detector/feature_extractor/__init__.py
+++ anomaly_detector/feature_extractor/__init__.py
@@ -5,12 +5,12 @@ from featureExtractorEfficientNetB0                 import FeatureExtractorEffic
 from featureExtractorEfficientNetB0_Block10         import FeatureExtractorEfficientNetB0_Block10
 
 from featureExtractorEfficientNetB6                 import FeatureExtractorEfficientNetB6
-from featureExtractorEfficientNetB6_Block10         import FeatureExtractorEfficientNetB6_Block10
+# from featureExtractorEfficientNetB6_Block10         import FeatureExtractorEfficientNetB6_Block10
 
 from featureExtractorResNet50V2                     import FeatureExtractorResNet50V2
 from featureExtractorResNet50V2_Block6              import FeatureExtractorResNet50V2_Block6
-from featureExtractorResNet50V2_LargeImage          import FeatureExtractorResNet50V2_LargeImage
-from featureExtractorResNet50V2_Block6_LargeImage   import FeatureExtractorResNet50V2_Block6_LargeImage
+# from featureExtractorResNet50V2_LargeImage          import FeatureExtractorResNet50V2_LargeImage
+# from featureExtractorResNet50V2_Block6_LargeImage   import FeatureExtractorResNet50V2_Block6_LargeImage
 
 from featureExtractorVGG16                          import FeatureExtractorVGG16
 from featureExtractorVGG16_Block4                   import FeatureExtractorVGG16_Block4
@@ -18,4 +18,4 @@ from featureExtractorVGG16_Block3                   import FeatureExtractorVGG16
 
 from featureExtractorC3D                            import FeatureExtractorC3D
 from featureExtractorC3D_Block4                     import FeatureExtractorC3D_Block4
-from featureExtractorC3D_Block3                     import FeatureExtractorC3D_Block3
+# from featureExtractorC3D_Block3                     import FeatureExtractorC3D_Block3     # Too large (73Gb for the whole dataset)
