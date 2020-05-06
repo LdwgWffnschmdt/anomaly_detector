fe36b4716f4f5110188f583e48a5f99b8300ef7b
LdwgWffnschmdt <LudwigWW@live.com>
2020-05-06 19:48:31 +0200

    Extractor benchmark now spawns dedicated processes to free GPU RAM

--- anomaly_detector/scripts/feature_extractor_benchmark.py
+++ anomaly_detector/scripts/feature_extractor_benchmark.py
@@ -13,6 +13,9 @@ parser.add_argument("--files", metavar="F", dest="files", type=str, default=cons
 parser.add_argument("--extractor", metavar="EXT", dest="extractor", nargs='*', type=str,
                     help="Extractor name. Leave empty for all extractors (default: \"\")")
 
+parser.add_argument("--output", metavar="OUT", dest="output", type=str,
+                    help="Output file (default: \"\")")
+
 parser.add_argument("--batch_sizes", metavar="B", dest="batch_sizes", nargs="*", type=int, default=[4,8,16,32,64,128,256,512],
                     help="Batch size for testing batched extraction. (default: [8,16,32,64,128,256,512])")
 
@@ -40,6 +43,7 @@ import numpy as np
 import xlsxwriter
 import tensorflow as tf
 from tqdm import tqdm
+import subprocess
 
 import feature_extractor
 
@@ -63,45 +67,63 @@ def feature_extractor_benchmark():
     if not files or len(files) < 1 or files[0] == "":
         raise ValueError("Please specify at least one filename (%s)" % files)
         
-
+    if args.output is None:
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), datetime.now().strftime("%Y_%m_%d_%H_%M_benchmark.xlsx"))
+    else:
+        filename = args.output
+    
     # Create a workbook and add a worksheet for meta data.
-    workbook = xlsxwriter.Workbook(os.path.join(os.path.dirname(os.path.realpath(__file__)), datetime.now().strftime("%Y_%m_%d_%H_%M_benchmark.xlsx")))
+    workbook = xlsxwriter.Workbook(filename)
     heading_format = workbook.add_format({'bold': True, 'font_color': '#0071bc', "font_size": 20})
     subheading_format = workbook.add_format({'bold': True, "font_size": 12})
 
-    #Write metadata
-    worksheet_meta = workbook.add_worksheet("Meta")
-    worksheet_meta.set_column(0, 0, 25)
-    worksheet_meta.set_column(1, 1, 40)
+    try:
+        #Write metadata
+        worksheet_meta = workbook.add_worksheet("Meta")
+        worksheet_meta.set_column(0, 0, 25)
+        worksheet_meta.set_column(1, 1, 40)
 
-    
-    def add_meta(n, s="", format=None):
-        global row
-        worksheet_meta.write(row, 0, n, format)
-        worksheet_meta.write(row, 1, s, format)
-        row += 1
+        
+        def add_meta(n, s="", format=None):
+            global row
+            worksheet_meta.write(row, 0, n, format)
+            worksheet_meta.write(row, 1, s, format)
+            row += 1
 
-    add_meta("Feature extractor benchmark", format=heading_format)
+        add_meta("Feature extractor benchmark", format=heading_format)
 
-    add_meta("Start", datetime.now().strftime("%d.%m.%Y, %H:%M:%S"))
-    add_meta("Batch size", str(args.batch_sizes))
+        add_meta("Start", datetime.now().strftime("%d.%m.%Y, %H:%M:%S"))
+        add_meta("Batch size", str(args.batch_sizes))
 
-    computer_info = utils.getComputerInfo()
+        computer_info = utils.getComputerInfo()
 
-    for key, value in computer_info.items():
-        add_meta(key, value)
+        for key, value in computer_info.items():
+            add_meta(key, value)
+    except:
+        pass
 
     # Get all the available feature extractor names
-    extractor_names = map(lambda e: e[0], inspect.getmembers(feature_extractor, inspect.isclass))
-    extractor_names = filter(lambda f: f != "FeatureExtractorBase", extractor_names)
 
     if args.extractor is None:
+        extractor_names = map(lambda e: e[0], inspect.getmembers(feature_extractor, inspect.isclass))
+        extractor_names = filter(lambda f: f != "FeatureExtractorBase", extractor_names)
         args.extractor = extractor_names
 
     # Get an instance of each class
     module = __import__("feature_extractor")
     
-    for extractor_name in tqdm(args.extractor, desc="Benchmarking extractors", file=sys.stderr):
+    if len(args.extractor) > 1:
+        workbook.close()
+        for extractor_name in tqdm(args.extractor, desc="Benchmarking extractors", file=sys.stderr):
+            command = "/home/ldwg/anomaly_detector/.env/bin/python /home/ldwg/anomaly_detector/anomaly_detector/scripts/feature_extractor_benchmark.py --extractor %s --output %s" % (extractor_name, filename)
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+            for line in process.stdout:
+                tqdm.write(line)
+            process.wait()
+    else:
+        extractor_name = args.extractor[0]
+        logger.info("Benchmarking %s" % extractor_name)
+
         worksheet = workbook.add_worksheet(extractor_name.replace("FeatureExtractor", ""))
         worksheet.set_column(0, 20, 20)
 
@@ -132,7 +154,12 @@ def feature_extractor_benchmark():
             _class = getattr(module, extractor_name)
 
             # Test extractor initialization
-            log("Initialization", np.array(timeit.repeat(lambda: _class(), number=1, repeat=args.init_repeat)))
+            try:
+                log("Initialization", np.array(timeit.repeat(lambda: _class(), number=1, repeat=args.init_repeat)))
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                logerr("Initialization", traceback.format_exc())
 
             extractor = _class()
     
@@ -145,7 +172,7 @@ def feature_extractor_benchmark():
                 raise ValueError("Supported file types are *.tfrecord and *.jpg")
             
             dataset = dataset.map(lambda image, time: (extractor.format_image(image), time),
-                                  num_parallel_calls=tf.data.experimental.AUTOTUNE)
+                                num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
             # Call internal transformations (eg. temporal windowing for 3D networks)
             # dataset = extractor.__transform_dataset__(dataset)
@@ -179,10 +206,11 @@ def feature_extractor_benchmark():
             log("Extraction minima", mins)
         except KeyboardInterrupt:
             logger.info("Cancelled")
-            break
+            raise
         except:
             logerr("Error?", traceback.format_exc())
-    workbook.close()
+            
+        workbook.close()
 
 if __name__ == "__main__":
     feature_extractor_benchmark()
